06e22a53d3b63e72322f461754ca294ef2b334fa
denizintepe <denizintepe@gmail.com>
2023-09-07 09:10:42 +0300

    Initial Commit

---
+++ WidgetTest/WidgetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestApp.swift
+//  WidgetTest
+//
+//  Created by deniz intepe on 7.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
